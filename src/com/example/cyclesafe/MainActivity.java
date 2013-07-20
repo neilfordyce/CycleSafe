39c26ebbea8ef58ee116d7e613a5302a143ccced
JackG188 <j.v.graham@dundee.ac.uk>
2013-07-20 18:29:26 +0100

    Added Button to activate GPS service

--- src/com/example/cyclesafe/MainActivity.java
+++ src/com/example/cyclesafe/MainActivity.java
@@ -5,9 +5,16 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
+	Button start, stop;
+	Boolean riding;
+	Intent gpsService;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) 
     {
@@ -15,8 +22,12 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
         
-        Intent gpsService = new Intent(getApplicationContext(), ServiceServer.class);
-        startService(gpsService);
+        start = (Button) findViewById(R.id.btn_LetsRide);
+        start.setOnClickListener(this);
+        riding=false;
+        
+        gpsService = new Intent(getApplicationContext(), ServiceServer.class);
+        
     }
 
 
@@ -26,5 +37,35 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		switch (arg0.getId()){
+
+		case R.id.btn_LetsRide:
+			
+		 
+		 
+		 if(riding==false)
+		 {
+			 //start gps yo
+			 riding =true;
+			 startService(gpsService);
+			 start.setBackgroundResource(R.drawable.custom_button_stop);
+			 start.setText("FINISHED RIDING");
+		 }
+		 else if (riding ==true)
+		 {
+			 //end gps yo
+			 riding = false;
+			 start.setBackgroundResource(R.drawable.custom_button_start);
+			 start.setText("LET'S RIDE");
+		 }
+		break;
+		}
+		
+	}
     
 }
